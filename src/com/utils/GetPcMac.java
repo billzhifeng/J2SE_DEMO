eed30b85660f2a25e35ab97af2ffd00b30a9187c
billzhifeng <billzhifeng@qq.com>
2021-01-07 11:13:43 +0800

    Update GetPcMac.java

--- src/com/utils/GetPcMac.java
+++ src/com/utils/GetPcMac.java
@@ -1,11 +1,5 @@
 
-/*
- * Copyright 2018 tuhu.cn All right reserved. This software is the
- * confidential and proprietary information of tuhu.cn ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Tuhu.cn
- */
+
 package com.utils;
 
 import java.net.InetAddress;
@@ -14,7 +8,7 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 /**
- * @author wangzhifeng
+ * @author 
  * @date 2018年3月22日 下午5:21:50
  */
 public class GetPcMac {
